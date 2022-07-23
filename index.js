abe830104f20109113c2178dadf4b57622f69ed3
romariojsx <romariojdosantos@gmail.com>
2022-07-23 20:44:29 -0300

    new pass

--- index.js
+++ index.js
@@ -33,4 +33,4 @@ app.get("/games", (req, res) => {
   });
 });
 
-app.listen(process.env.PORT || 3000, () => console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env));
+app.listen(process.env.PORT || 3000, () => console.log("Express server listening on port mode"));
