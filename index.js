997f769c743551c834636e0ae571a3f1c3f0db4f
Romário de Jesus dos Santos <romariojdosantos@gmail.com>
2022-07-23 17:51:55 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const mysql = require("mysql2");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const app = express();
+const porta = 3333;
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+require('dotenv').config();
+const db = mysql.createConnection(process.env.DATABASE_URL);
+console.log('Connected to PlanetScale!');
+db.end();
+
+app.post("/get", (req, res) => {
+  const game = req.body.Game
+  const price = req.body.Price;
+  const category = req.body.Category;
+
+  const SQL = "INSERT INTO games (`game`, `cost`, `category`) VALUES (?, ?, ?)";
+  db.query(SQL, [game, price, category], (err, results) => {
+    if (err) throw console.log(err);
+    else {
+      results;
+    }
+  })
+});
+
+app.get("/games", (req, res) => {
+  db.query("SELECT * FROM games", (err, results) => {
+    res.send(results);
+  });
+});
+
+app.listen(porta, () => console.log(`Servidor rodando na porta ${porta}`));
