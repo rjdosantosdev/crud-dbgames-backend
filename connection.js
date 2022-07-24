75c3898992abaa3a5c068903c343ccd9e4896c2c
romariojsx <romariojdosantos@gmail.com>
2022-07-24 19:29:57 -0300

    Update connection.js

--- connection.js
+++ connection.js
@@ -1,9 +1,9 @@
 const mysql = require("mysql2");
 const connection = mysql.createConnection({
   database: "dbgames",
-  user: "i5yal2hktajs",
+  user: "l8zhjw7e2sx5",
   host: "eip4muxi5i2r.aws-sa-east-1-1.psdb.cloud",
-  password: "pscale_pw_aA7Y3PkTQRS5fJOb6pBkrR--y5eBB1FSt3n25g6R8n0",
+  password: "pscale_pw_qapjOt6XGzhwH4kefmtKErTK0szbLYbacbVixnYBgx4",
   ssl: {
     require: true,
     rejectUnauthorized: false,
