f4e68ce97503bee3ec6eb3ac64d6ff9220656772
romariojsx <romariojdosantos@gmail.com>
2022-07-24 19:21:42 -0300

    malditas senhas

--- connection.js
+++ connection.js
@@ -1,9 +1,9 @@
 const mysql = require("mysql2");
 const connection = mysql.createConnection({
   database: "dbgames",
-  user: "6y3514rb6mc4",
+  user: "i5yal2hktajs",
   host: "eip4muxi5i2r.aws-sa-east-1-1.psdb.cloud",
-  password: "pscale_pw_YmQH35iXqD8odwoWO3Egnl-pxvxr8j6RuruU-_crtuY",
+  password: "pscale_pw_aA7Y3PkTQRS5fJOb6pBkrR--y5eBB1FSt3n25g6R8n0",
   ssl: {
     require: true,
     rejectUnauthorized: false,
