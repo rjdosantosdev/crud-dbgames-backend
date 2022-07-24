43a79194af23295064f3217f0519efbf79b3019d
Romário de Jesus dos Santos <romariojdosantos@gmail.com>
2022-07-24 00:42:25 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 53324;
 
 app.use(cors());
 app.use(bodyParser.json());
