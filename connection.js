fb675a4127bc02c6c1324e78bc828b219713f920
romariojsx <romariojdosantos@gmail.com>
2022-07-24 21:31:29 -0300

    up

--- connection.js
+++ connection.js
@@ -1,13 +1,7 @@
 const mysql = require("mysql2");
-const connection = mysql.createConnection({
-  database: "dbgames",
-  user: "2cbc831rg694",
-  host: "eip4muxi5i2r.aws-sa-east-1-1.psdb.cloud",
-  password: "pscale_pw_Wj4NmQ9bTzr4G3ruqJb4TJcYz8jaHFy-RTS4E5WbqUE",
-  ssl: {
-    require: true,
-    rejectUnauthorized: false,
-  },
-});
+require('dotenv').config();
+const connection = mysql.createConnection(process.env.DATABASE_URL);
+console.log('Connected to PlanetScale!');
+connection.end();
 
 module.exports = connection;
