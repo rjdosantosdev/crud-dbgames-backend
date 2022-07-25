4366856b111c93e56ac09c90c923d57bd6d2ec14
romariojsx <romariojdosantos@gmail.com>
2022-07-24 21:34:43 -0300

    connect

--- connection.js
+++ connection.js
@@ -2,6 +2,6 @@ const mysql = require("mysql2");
 require('dotenv').config();
 const connection = mysql.createConnection(process.env.DATABASE_URL);
 console.log('Connected to PlanetScale!');
-connection.end();
+connection.connect();
 
 module.exports = connection;
